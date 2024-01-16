bd6f488e9c0f3e01fb79dc7a81516586a2789ded
Duy Do <sieuanhhung98@gmail.com>
2024-01-17 01:32:21 +0700

    Login and Show name when is logged

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     API_URL: "http://localhost:3000",
     DB_LOCAL_URI: "mongodb://localhost:27017/bookit-v2",
     DB_URI: "",
+
+    NEXTAUTH_URL: "http://localhost:3000",
+    NEXTAUTH_SECRET: "DJE9CMVLWKFPDWOQPMCAJVKQPEKOIOHNCAVMMWJK",
   },
   images: {
     remotePatterns: [
